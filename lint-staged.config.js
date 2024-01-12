a3febab3e86be85d57501edb074c68d22b90857d
John Yasis <johnamielyasis@gmail.com>
2024-01-11 16:31:31 -0800

    fix: change order to run prettier first then eslint to fix autoformat issues

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,10 +2,10 @@ module.exports = {
   // this will check Typescript files
   "**/*.(ts|tsx)": () => "bun tsc --noEmit",
 
-  // This will lint and format TypeScript and                                             //JavaScript files
+  // This will lint and format TypeScript and JavaScript files
   "**/*.(ts|tsx|js)": (filenames) => [
-    `bun eslint --fix ${filenames.join(" ")}`,
     `bun prettier --write ${filenames.join(" ")}`,
+    `bun eslint --fix ${filenames.join(" ")}`,
   ],
 
   // this will Format MarkDown and JSON
