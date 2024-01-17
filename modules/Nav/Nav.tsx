9785568ad24208b81feda542190363858e3ab219
tammyxa <icecoldxD123@gmail.com>
2024-01-17 10:28:09 -0800

    fix: nav spacing

--- modules/Nav/Nav.tsx
+++ modules/Nav/Nav.tsx
@@ -2,13 +2,20 @@ import { FluidContainer, Typography } from "@/components";
 import Image from "next/image";
 export const Nav = () => {
   return (
-    <FluidContainer flex justifyContent="between" color="black">
-      <Image alt="SLE Logo" src="/sle-logo.png" width={130} height={70}></Image>
-      <div className="flex flex-row items-center">
-        <Typography variant="navHeader">Vote</Typography>
-        <Typography variant="navHeader">ASI</Typography>
-        <Typography variant="navHeader">U-SU</Typography>
-      </div>
-    </FluidContainer>
+    <div className="bg-black">
+      <FluidContainer flex justifyContent="between" color="black">
+        <Image
+          alt="SLE Logo"
+          src="/sle-logo.png"
+          width={130}
+          height={70}
+        ></Image>
+        <div className="flex flex-row items-center">
+          <Typography variant="navHeader">Vote</Typography>
+          <Typography variant="navHeader">ASI</Typography>
+          <Typography variant="navHeader">U-SU</Typography>
+        </div>
+      </FluidContainer>
+    </div>
   );
 };
