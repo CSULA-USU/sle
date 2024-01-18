46df587d2e4f03327db16c7376b31dd4ae4f294f
John Yasis <johnamielyasis@gmail.com>
2024-01-17 17:39:45 -0800

    feat: unopinionated modal

---
+++ modules/PositionDescriptions/PositionDescriptions.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { useState } from "react";
+import { Typography, Modal } from "@/components";
+
+export const PositionDescriptions = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div className="my-10">
+      <div className="cursor-pointer">
+        <button onClick={openModal}>openmodelo</button>
+        <Modal isOpen={modalOpen} onClose={closeModal}>
+          <Typography>insert chilrin here </Typography>
+        </Modal>
+      </div>
+    </div>
+  );
+};
