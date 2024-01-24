ab841a56bf5a0b8292a404cedcdd69f70dc1c9ef
tammyxa <icecoldxD123@gmail.com>
2024-01-23 16:48:26 -0800

    chore: update accent color

--- components/Divider/Divider.tsx
+++ components/Divider/Divider.tsx
@@ -7,7 +7,7 @@ export const Divider = ({ margin, color }: DividerProps) => {
   return (
     <hr
       className={`w-full h-px border-2 border-${
-        color === "gold" ? "[#C59F6C]" : "yellow-400"
+        color === "gold" ? "[#C6A159]" : "yellow-400"
       } ${margin === "none" ? "my-0" : margin === "reg" ? "my-5" : "my-10"}`}
     ></hr>
   );
