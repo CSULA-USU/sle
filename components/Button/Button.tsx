1a1b6966eef37e524ee60745994932631e7910d0
John Yasis <johnamielyasis@gmail.com>
2025-12-12 15:20:01 -0800

    fix: Button and Modal accessibility fixes

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -18,38 +18,31 @@ export const Button = ({
   isDisabled,
   isExternalLink,
 }: ButtonProps) => {
+  const baseStyles = `inline-block rounded-md py-3 px-4 hover:opacity-70 ${
+    variant === "black"
+      ? "text-white bg-black"
+      : variant === "yellow"
+        ? "bg-yellow-400 border-none"
+        : "bg-gray-300 border-none"
+  } ${borderless ? "border-none" : ""}`;
+
   return (
     <>
       {href && !isDisabled ? (
         <div className="my-4">
           <NonBreakingSpan>
-            <span
-              className={`rounded-md py-3 px-4 hover:opacity-70 ${
-                variant === "black"
-                  ? "text-white bg-black"
-                  : variant === "yellow"
-                    ? "bg-yellow-400 border-none"
-                    : "bg-gray-300 border-none"
-              } ${borderless ? "border-none" : ""} {class}`}
+            <a
+              href={href}
+              target={isExternalLink ? "_blank" : "_self"}
+              rel={isExternalLink ? "noopener noreferrer" : undefined}
+              className={baseStyles}
             >
-              <a href={href} target={isExternalLink ? "_blank" : "_self"}>
-                <Typography variant="buttonText">{text}</Typography>
-              </a>
-            </span>
+              <Typography variant="buttonText">{text}</Typography>
+            </a>
           </NonBreakingSpan>
         </div>
       ) : (
-        <button
-          className={`rounded-md hover:opacity-70 ${
-            variant === "black"
-              ? "text-white bg-black"
-              : variant === "yellow"
-                ? "bg-yellow-400 border-none"
-                : "bg-gray-300 border-none"
-          } ${borderless ? "border-none" : ""} py-2 px-4 {class}`}
-          disabled={isDisabled}
-          aria-disabled={isDisabled}
-        >
+        <button className={baseStyles} disabled={isDisabled}>
           <Typography variant="buttonText">{text}</Typography>
         </button>
       )}
